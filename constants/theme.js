478c2134bea05f361bbb4baac30ced74e415245a
KM ASKAR <115799485+km11askar@users.noreply.github.com>
2025-07-11 14:01:40 +0530

    Add files via upload

---
+++ constants/theme.js
@@ -0,0 +1,62 @@
+
+
+// export const theme = {
+//     Colors:{
+//         primary:'#00c26f',
+//         primaryDark:'#00AC62',
+//         dark:'#3E3E3E',
+//         darkLight:'#E1E1E1',
+//         gray:'#e3e3e3',
+
+//         text:'#494949',
+//         textLight:'#7C7C7C',
+//         textDark:'#1D1D1D',
+
+//         rose:'#ef4444',
+//         roseLight:'#f87171',
+    
+//     },
+//     fonts:{
+//         medium:'500',
+//         semiBold:'600',
+//         bold:'700',
+//         extraBold:'800',
+//     },
+//     radius:{
+//         xs:10,
+//         sm:12,
+//         md:14,
+//         lg:16,
+//         xl:18,
+//         xxl:22,
+//     }
+// }
+
+export const theme = {
+    Colors: {
+      primary: '#00c26f',
+      primaryDark: '#00AC62',
+      dark: '#3E3E3E',
+      darkLight: '#E1E1E1',
+      gray: '#e3e3e3',
+      text: '#494949',
+      textLight: '#7C7C7C',
+      textDark: '#1D1D1D',
+      rose: '#ef4444',
+      roseLight: '#f87171',
+    },
+    fonts: {
+      medium: '500',
+      semiBold: '600',
+      bold: '700',
+      extraBold: '800',
+    },
+    radius: {
+      xs: 10,
+      sm: 12,
+      md: 14,
+      lg: 16,
+      xl: 18,
+      xxl: 22,
+    }
+  };
