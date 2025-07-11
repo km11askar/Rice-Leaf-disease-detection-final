57399685ab502b2496845e1b24160c7c95618799
KM ASKAR <115799485+km11askar@users.noreply.github.com>
2025-07-11 14:01:57 +0530

    Add files via upload

---
+++ config/firebase.js
@@ -0,0 +1,23 @@
+// import { initializeApp } from "firebase/app";
+// import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB6clD3rLjWRlo1shzVeY2J1ihM7IrfugE",
+//   authDomain: "rldd-50134.firebaseapp.com",
+//   projectId: "rldd-50134",
+//   storageBucket: "rldd-50134.appspot.com",
+//   messagingSenderId: "610790353896",
+//   appId: "1:610790353896:web:2780938e7e7ee9d997525d"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Initialize Auth
+// const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(AsyncStorage)
+// });
+
+// export { auth };
